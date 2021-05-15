698b7db57c51f6c6907cbad84a83b27563dc30e2
Ju99ernaut <brendonzee@gmail.com>
2021-05-15 20:11:09 +0200

    Add mail utils

---
+++ api/mail/send.py
@@ -0,0 +1,40 @@
+import os
+
+from typing import List
+from pydantic import EmailStr
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+
+# from fastapi_mail.email_utils import DefaultChecker
+
+import config
+
+config.parse_args()
+
+
+conf = ConnectionConfig(
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME") or config.CONFIG.mail_username,
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD") or config.CONFIG.mail_password,
+    MAIL_FROM=os.getenv("MAIL_USERNAME")
+    or config.CONFIG.mail_username
+    or "user@example.com",
+    MAIL_PORT=int(config.CONFIG.mail_port),
+    MAIL_SERVER=config.CONFIG.mail_server,
+    MAIL_FROM_NAME=config.CONFIG.mail_from,
+    MAIL_TLS=True,
+    MAIL_SSL=False,
+    USE_CREDENTIALS=True,
+)
+
+
+async def newsletter(to: List[EmailStr], subject: str, html: str):
+    message = MessageSchema(
+        subject=subject,
+        recipients=to,
+        body=html,
+        subtype="html",
+    )
+    if (os.getenv("MAIL_USERNAME") and os.getenv("MAIL_PASSWORD")) or (
+        config.CONFIG.mail_username and config.CONFIG.mail_password
+    ):
+        fm = FastMail(conf)
+        await fm.send_message(message)
