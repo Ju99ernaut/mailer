aec12b3d172db846445ee5e9e8235535a9f85bcb
Ju99ernaut <brendonzee@gmail.com>
2021-05-28 19:55:00 +0200

    Update editor

--- api/grapesjs/editor.py
+++ api/grapesjs/editor.py
@@ -1,3 +1,5 @@
+import data
+
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -12,6 +14,13 @@ templates = Jinja2Templates(directory="api/templates")
 
 @router.get("", response_class=HTMLResponse)
 async def get_editor(request: Request):
+    uppy_config = data.get_uppy_config() or {"companion_url": "", "endpoint": ""}
     return templates.TemplateResponse(
-        "editor.html", {"request": request, "pfx": config.CONFIG.prefix}
+        "editor.html",
+        {
+            "request": request,
+            "pfx": config.CONFIG.prefix,
+            "companion_url": uppy_config["companion_url"],
+            "endpoint": uppy_config["endpoint"],
+        },
     )
