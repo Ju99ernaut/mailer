4ecf3241a6faab780ac1388a60a741a3ae997609
Ju99ernaut <brendonzee@gmail.com>
2021-05-29 10:32:18 +0200

    Update send.py

--- api/mail/send.py
+++ api/mail/send.py
@@ -6,30 +6,23 @@ from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
 
 # from fastapi_mail.email_utils import DefaultChecker
 
-current_config = data.get_campaign_config() or {
-    "MAIL_USERNAME": "",
-    "MAIL_PASSWORD": "",
-    "MAIL_FROM": "user@example.com",
-    "MAIL_PORT": 587,
-    "MAIL_SERVER": "smtp.gmail.com",
-    "MAIL_FROM_NAME": "Newsletter",
-    "MAIL_TLS": True,
-    "MAIL_SSL": False,
-    "USE_CREDENTIALS": True,
-}
 
-conf = ConnectionConfig(
-    MAIL_USERNAME=current_config["MAIL_USERNAME"] or "",
-    MAIL_PASSWORD=current_config["MAIL_PASSWORD"] or "",
-    MAIL_FROM=current_config["MAIL_FROM"] or "user@example.com",
-    MAIL_PORT=current_config["MAIL_PORT"],
-    MAIL_SERVER=current_config["MAIL_SERVER"],
-    MAIL_FROM_NAME=current_config["MAIL_FROM_NAME"],
-    MAIL_TLS=current_config["MAIL_TLS"],
-    MAIL_SSL=current_config["MAIL_SSL"],
-    USE_CREDENTIALS=current_config["USE_CREDENTIALS"],
-    TEMPLATE_FOLDER="./api/templates",
-)
+def get_config():
+    current_config = data.get_campaign_config_default()
+    if not current_config:
+        return False
+    return ConnectionConfig(
+        MAIL_USERNAME=current_config["MAIL_USERNAME"],
+        MAIL_PASSWORD=current_config["MAIL_PASSWORD"],
+        MAIL_FROM=current_config["MAIL_FROM"],
+        MAIL_PORT=current_config["MAIL_PORT"],
+        MAIL_SERVER=current_config["MAIL_SERVER"],
+        MAIL_FROM_NAME=current_config["MAIL_FROM_NAME"],
+        MAIL_TLS=current_config["MAIL_TLS"],
+        MAIL_SSL=current_config["MAIL_SSL"],
+        USE_CREDENTIALS=current_config["USE_CREDENTIALS"],
+        TEMPLATE_FOLDER="./api/templates",
+    )
 
 
 async def newsletter(to: List[EmailStr], subject: str, body: str):
@@ -39,7 +32,8 @@ async def newsletter(to: List[EmailStr], subject: str, body: str):
         body=body,
         subtype="html",
     )
-    if current_config["MAIL_USERNAME"] and current_config["MAIL_PASSWORD"]:
+    conf = get_config()
+    if conf:
         fm = FastMail(conf)
         await fm.send_message(message)
 
@@ -51,6 +45,7 @@ async def use_template(to: List[EmailStr], subject: str, body: dict, template: s
         body=body,
         subtype="html",
     )
-    if current_config["MAIL_USERNAME"] and current_config["MAIL_PASSWORD"]:
+    conf = get_config()
+    if conf:
         fm = FastMail(conf)
         await fm.send_message(message, template=template)
