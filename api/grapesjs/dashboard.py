ab199e0e34ad869547f8abdf187694ed0e0bb49e
Ju99ernaut <brendonzee@gmail.com>
2021-06-06 13:36:19 +0200

    Update dashboard.py

--- api/grapesjs/dashboard.py
+++ api/grapesjs/dashboard.py
@@ -1,3 +1,5 @@
+import data
+
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -10,4 +12,18 @@ templates = Jinja2Templates(directory="api/templates")
 
 @router.get("", response_class=HTMLResponse)
 async def get_dashboard(request: Request):
-    return templates.TemplateResponse("dashboard.html", {"request": request})
+    campaign_config = data.get_campaign_config_default()
+    campaigns = data.get_all_campaigns_unpaginated()
+    subscribers = data.get_all_emails_unpaginated()
+    uppy_config = data.get_uppy_config()
+    return templates.TemplateResponse(
+        "dashboard.html",
+        {
+            "request": request,
+            "user": {"username": "username"},
+            "campaign_config": campaign_config,
+            "campaigns": campaigns,
+            "subscribers": subscribers,
+            "uppy_config": uppy_config,
+        },
+    )
