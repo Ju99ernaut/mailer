f23def8f5db01e2ac2e7c6dbd3b1d676b796f2de
Ju99ernaut <brendonzee@gmail.com>
2021-06-06 13:35:25 +0200

    Update router

--- api/static/js/router.js
+++ api/static/js/router.js
@@ -1,5 +1,10 @@
 var icons = document.querySelectorAll('.left-side a svg'),
     subs = document.querySelectorAll('.header-link'),
+    box1 = document.querySelector('.first-box'),
+    box2 = document.querySelector('.second-box'),
+    box3 = document.querySelector('.third-box'),
+    box4 = document.querySelector('.fourth-box'),
+    box5 = document.querySelector('.fifth-box'),
     homeEl = icons[0],
     mailEl = icons[1],
     settingsEl = icons[2],
@@ -32,8 +37,13 @@ function home() {
     deactivateAll(icons);
     deactivateAll(subs);
     hide(subs);
-    // TODO Hide All
-    // TODO Show home content
+    // * Hide All
+    box3.style.display = 'none';
+    box4.style.display = 'none';
+    box5.style.display = 'none';
+    // * Show home content
+    box1.style.display = 'flex';
+    box2.style.display = 'flex';
     activate(homeEl);
 }
 
@@ -41,8 +51,13 @@ function mail() {
     deactivateAll(icons);
     deactivateAll(subs);
     hide(subs);
-    // TODO Hide All
-    // TODO Show mail content
+    // * Hide All
+    box1.style.display = 'none';
+    box2.style.display = 'none';
+    box4.style.display = 'none';
+    box5.style.display = 'none';
+    // * Show mail content
+    box3.style.display = 'flex';
     activate(mailEl);
 }
 
@@ -50,8 +65,21 @@ function settings(subEl) {
     deactivateAll(icons);
     deactivateAll(subs);
     show(subs);
-    // TODO Hide All
-    // TODO Show settings content
+    // * Hide All
+    box1.style.display = 'none';
+    box2.style.display = 'none';
+    box3.style.display = 'none';
+    // * Show settings content
+    if (subEl && subEl === 'config') {
+        box4.style.display = 'flex';
+        box5.style.display = 'none';
+    } else if (subEl && subEl === 'uppy') {
+        box4.style.display = 'none';
+        box5.style.display = 'flex';
+    } else {
+        box4.style.display = 'none';
+        box5.style.display = 'none';
+    }
     activate(settingsEl, document.querySelector('.' + subEl));
 }
 
